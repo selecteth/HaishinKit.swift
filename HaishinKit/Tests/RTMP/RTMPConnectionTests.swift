b3e316922f7913a4ffc3aa47d1cea413b7ca0419
shogo4405 <shogo4405@gmail.com>
2025-07-07 21:37:36 +0900

    Update RTMPConnectionTests.

--- HaishinKit/Tests/RTMP/RTMPConnectionTests.swift
+++ HaishinKit/Tests/RTMP/RTMPConnectionTests.swift
@@ -4,24 +4,14 @@ import Testing
 @testable import HaishinKit
 
 @Suite struct RTMPConnectionTests {
-    /*
-     func publish() {
-     let connection = RTMPConnection()
-     let stream = RTMPStream(connection: connection)
-     connection.connect("rtmp://localhost:1935/live")
-     stream.publish("live")
-     sleep(10000)
-     }
-
-     func testReleaseWhenClose() {
-     weak var weakConnection: RTMPConnection?
-     _ = {
-     let connection = RTMPConnection()
-     connection.connect("rtmp://localhost:1935/live")
-     connection.close()
-     weakConnection = connection
-     }()
-     XCTAssertNil(weakConnection)
-     }
-     */
+    @Test func releaseWhenClose() async throws {
+        weak var weakConnection: RTMPConnection?
+        _ = try? await {
+            let connection = RTMPConnection()
+            _ = try await connection.connect("rtmp://localhost:1935/live")
+            try await connection.close()
+            weakConnection = connection
+        }()
+        #expect(weakConnection == nil)
+    }
 }
