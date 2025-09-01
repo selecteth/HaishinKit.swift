8cc18fd66c09a6934612fa7c464c1a584704bacc
shogo4405 <shogo4405@gmail.com>
2025-09-01 20:42:28 +0900

    Update Examples visionOS.

--- Examples/visionOS/ContentView.swift
+++ Examples/visionOS/ContentView.swift
@@ -4,27 +4,9 @@ import SRTHaishinKit
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var viewModel = ViewModel()
-
-    private var lfView: PiPHKSwiftUiView!
-
-    init() {
-        viewModel.config()
-        lfView = PiPHKSwiftUiView(rtmpStream: $viewModel.stream)
-        Task {
-            await SessionBuilderFactory.shared.register(RTMPSessionFactory())
-            await SessionBuilderFactory.shared.register(SRTSessionFactory())
-        }
-    }
-
     var body: some View {
         VStack {
-            lfView
-                .ignoresSafeArea()
-                .onTapGesture { _ in
-                    self.viewModel.startPlaying()
-                }
-            Text("Hello, world!")
+            PlaybackView()
         }
     }
 }
