69b00437962849a2ba439798af69fe866bfcb6d2
shogo4405 <shogo4405@gmail.com>
2025-09-17 02:12:48 +0900

    Update RTCLogger.swift for SwiftDoc.

--- RTCHaishinKit/Sources/RTC/RTCLogger.swift
+++ RTCHaishinKit/Sources/RTC/RTCLogger.swift
@@ -1,13 +1,22 @@
 import libdatachannel
 
+/// An actor for writing interpolated string messages to `libdatachannel` logging system.
 public actor RTCLogger {
+    /// Defines the logging severity levels supported by `libdatachannel`.
     public enum Level {
+        /// No logs will be emitted.
         case none
+        /// Fatal errors.
         case fatal
+        /// Recoverable errors.
         case error
+        /// Potential issues that should be noted.
         case warning
+        /// General informational messages.
         case info
+        /// Debug messages for development and troubleshooting.
         case debug
+        /// Verbose messages for detailed tracing.
         case verbose
 
         var cValue: rtcLogLevel {
@@ -30,10 +39,13 @@ public actor RTCLogger {
         }
     }
 
+    /// The singleton logger instance.
     public static let shared = RTCLogger()
 
+    /// The current logging level.
     public private(set) var level: Level = .none
 
+    /// Sets the current logging level.
     public func setLevel(_ level: Level) {
         rtcInitLogger(level.cValue, nil)
     }
